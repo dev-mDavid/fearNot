4147e643574023c0b0cbdbe98c6c651dcc25c378
mDavid <dev.mdavid@gmail.com>
2020-02-11 17:42:28 -0800

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShareModule } from './share/share.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ShareModule } from './share/share.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ShareModule
+    ShareModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
