afe9f2511affcc9e447369e027a27276417aa363
mDavid <dev.mdavid@gmail.com>
2020-02-13 15:27:26 -0800

    create template input variable for component

--- src/app/mmt-question-textarea/mmt-question-textarea.component.ts
+++ src/app/mmt-question-textarea/mmt-question-textarea.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'mmt-question-textarea',
   templateUrl: './mmt-question-textarea.component.html',
   styleUrls: ['./mmt-question-textarea.component.scss']
 })
-export class MmtQuestionTextareaComponent implements OnInit {
 
+export class MmtQuestionTextareaComponent implements OnInit {
+  @Input() renderTitle: string;
+  
   constructor() { }
 
   ngOnInit(): void {
+
+
   }
 
 }
